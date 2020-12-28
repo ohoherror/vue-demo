54be86ee9b2ff1ef1cf4138f3d17a7ae99157b73
xuchang <1458251589@qq.com>
2020-12-28 14:22:35 +0800

    新增

---
+++ src/components/layout/layout.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-container>
+    <el-header>
+      <top-menu></top-menu>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <left-menu></left-menu>
+      </el-aside>
+      <el-main>
+        <el-container>
+          <el-header>
+            <head-menu></head-menu>
+          </el-header>
+          <el-main></el-main>
+        </el-container>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import topMenu from "../topMenu";
+import leftMenu from "../leftMenu";
+import headMenu from "../headMenu";
+export default {
+  data() {
+    return {};
+  },
+  component: {
+    topMenu,
+    leftMenu,
+    headMenu,
+  },
+};
+</script>
+
+<style></style>
